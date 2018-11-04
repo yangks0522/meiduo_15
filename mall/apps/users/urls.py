9d698e79223da1cebd98432ce13c12bbcfc50d2f
yks <yangks0522@163.com>
2018-11-04 21:14:22 +0800

    用户名的前段vue&html代码实现

--- mall/apps/users/urls.py
+++ mall/apps/users/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     # /users/usernames/(?P<username>\w{5,20})/count/
+    # url(r'^usernames/(?P<username>[a-zA-Z0-9_-]{5,20})/count/$', views.RegisterUsernameCountView.as_view(), name='usernamecount'),
     url(r'^usernames/(?P<username>\w{5,20})/count/$', views.RegisterUsernameCountView.as_view(), name='usernamecount'),
 
 ]
