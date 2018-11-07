62abc05b0d37407c188c0e749feda40873da0ece
yks <yangks0522@163.com>
2018-11-07 15:33:29 +0800

    create问题&序列化器的read_only和密码保存

--- mall/apps/users/serializers.py
+++ mall/apps/users/serializers.py
@@ -3,15 +3,19 @@ from users.models import User
 import re
 from django_redis import get_redis_connection
 
+
 # 入库有模型  选择ModelSerializer
 class RegisterCreateUserSerializer(serializers.ModelSerializer):
     """
     username, password, password2, mobile, sms_code, allow
+
+    自动写的字段也要添加到列表中
     """
 
-    sms_code = serializers.CharField(label='短信验证码', min_length=6, max_length=6, required=True)
-    password2 = serializers.CharField(label='确认密码', required=True)
-    allow = serializers.CharField(label='确认协议', required=True)
+    sms_code = serializers.CharField(label='短信验证码', min_length=6, max_length=6, write_only=True)
+    password2 = serializers.CharField(label='确认密码', write_only=True)
+    allow = serializers.CharField(label='确认协议', write_only=True)
+
     # ModelSerializer自动生成字段的时候 是根据fields 列表生成的
     class Meta:
         model = User
@@ -27,12 +31,12 @@ class RegisterCreateUserSerializer(serializers.ModelSerializer):
 
     def validate_mobile(self, value):
 
-        if not re.match('1[345789]\d{9}',value):
+        if not re.match('1[345789]\d{9}', value):
             raise serializers.ValidationError('手机号不满足要求')
         # 校验之后要返回回去
         return value
 
-    def validate_allow(self,value):
+    def validate_allow(self, value):
 
         if value == False:
             raise serializers.ValidationError('您未同意协议')
@@ -50,7 +54,7 @@ class RegisterCreateUserSerializer(serializers.ModelSerializer):
         # 短信
         redis_conn = get_redis_connection('code')
 
-        sms_code_redis = redis_conn.get("sms_%s"%mobile)
+        sms_code_redis = redis_conn.get("sms_%s" % mobile)
 
         if not sms_code_redis:
             raise serializers.ValidationError('验证码已过期')
@@ -58,5 +62,15 @@ class RegisterCreateUserSerializer(serializers.ModelSerializer):
         if sms_code_redis.decode() != sms_code:
             raise serializers.ValidationError('验证码错误')
 
-
         return attrs
+
+    def create(self, validated_data):
+        del validated_data['password2']
+        del validated_data['sms_code']
+        del validated_data['allow']
+
+        user = User.objects.create(**validated_data)
+        # 对密码进行加密
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
