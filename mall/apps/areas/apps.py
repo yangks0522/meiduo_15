ee8dec4f7373c9ba3d075ba21cd750cfa6caa44b
yks <yangks0522@163.com>
2018-11-10 21:01:37 +0800

    省市区模型

--- mall/apps/areas/apps.py
+++ mall/apps/areas/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class AreaConfig(AppConfig):
+class AreasConfig(AppConfig):
     name = 'areas'
--- mall/mall/settings.py
+++ mall/mall/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'oauth.apps.OauthConfig',
+    'areas.apps.AreasConfig',
 ]
 
 MIDDLEWARE = [
